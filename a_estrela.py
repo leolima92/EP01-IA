b88125bffec681518467ef835ccaebeddacefba4
Eduardo Leal Aruth <101681441+EduLeal@users.noreply.github.com>
2026-03-08 02:37:10 -0300

    feat: resto da função executar_a_estrela

--- a_estrela.py
+++ a_estrela.py
@@ -12,4 +12,22 @@ def executar_a_estrela(problema):
     heapq.heappush(fronteira, (f_raiz, id(no_raiz), no_raiz))
 
     visitados = {no_raiz.estado: no_raiz.custo}
-    return
+
+    total_visitados = 0
+
+    while fronteira:
+        f_n, _, no_atual = heapq.heappop(fronteira)
+        total_visitados += 1
+
+        if problema.objetivo(no_atual):
+            return no_atual, total_visitados
+        
+        for acao, no_filho in problema.sucessores(no_atual):
+            if no_filho.estado not in visitados or no_filho.custo < visitados[no_filho.estado]:
+                visitados[no_filho.estado] = no_filho.custo
+
+                no_filho.heuristica = problema.heuristica(no_filho)
+                f_filho = no_filho.custo + no_filho.heuristica
+
+                heapq.heappush(fronteira, (f_filho, id(no_filho), no_filho))
+        return None, total_visitados
