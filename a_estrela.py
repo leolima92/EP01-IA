3afeaf22881c5e702b36e2e711829175194387a9
Eduardo Leal Aruth <101681441+EduLeal@users.noreply.github.com>
2026-03-08 02:28:12 -0300

    feat: a_estrela inicial

--- a_estrela.py
+++ a_estrela.py
@@ -5,5 +5,11 @@ from utils import salvar_saida, caminho_saida_por_entrada
 
 def executar_a_estrela(problema):
     no_raiz = problema.iniciar()
-    
+    no_raiz.heuristica = problema.heuristica(no_raiz)
+
+    fronteira = []
+    f_raiz = no_raiz.custo + no_raiz.heuristica
+    heapq.heappush(fronteira, (f_raiz, id(no_raiz), no_raiz))
+
+    visitados = {no_raiz.estado: no_raiz.custo}
     return
